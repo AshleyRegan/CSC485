4809c0c5c60af3dbc5117c3122e6b8405178ff00
AshleyRegan <ashregan@zoho.com>
2015-11-16 21:53:41 -0500

    Boxes
    Not much functionality yet, will work on it, tomorrow

--- code.py
+++ code.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+import sqlite3 as lite
+import sys
+
 import web
 
 render = web.template.render('templates/')
@@ -9,13 +14,16 @@ urls = (
 
 class helloworld:
    def GET(self):
-
         return "Hello, world!"
 
+class add:
+    def POST(self):
+        i = web.input()
+        n = db.insert('ppl', username=i.un, password=i.pw)
+        raise web.seeother('/')
 
 class index:
    def GET(self):
-
         i = web.input(name=None)
         return render.index(i.name)
 
