0dcf59b215204de818319c38ae69a483851a4f8a
AshleyRegan <AshleyRegan@users.noreply.github.com>
2015-12-02 17:35:34 -0500

    adding a secret that can be exposed by attack

--- a1i/badversion/dbCreator.py
+++ a1i/badversion/dbCreator.py
@@ -8,11 +8,15 @@ import sys
 con = lite.connect('people.db')
 
 with con:
+    #creates a cursor to work with database
     cur = con.cursor()   
     #creates table & labels column ids
-    cur.execute("CREATE TABLE Ppl(id INT, username TEXT, password TEXT)")
+    cur.execute("CREATE TABLE Ppl(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, secret CHAR(100))")
     #populating the table
-    cur.execute('''INSERT INTO Ppl VALUES("user", "pass")''')
-    cur.execute('''INSERT INTO Ppl VALUES("admin", "123")''')
-    cur.execute('''INSERT INTO Ppl VALUES("can", "sux")''')
-    cur.execute('''INSERT INTO Ppl VALUES("johnny", "you")''')
+    cur.execute('''INSERT INTO Ppl VALUES("admin", "123", "I don't know what I'm doing")''')
+    cur.execute('''INSERT INTO Ppl VALUES("user", "pass", "I have no imagination")''')
+    cur.execute('''INSERT INTO Ppl VALUES("can", "sux", "This is not a secret")''')
+    cur.execute('''INSERT INTO Ppl VALUES("johnny", "you", "You're not actually johnny")''')
+
+con.commit()
+cur.close()
