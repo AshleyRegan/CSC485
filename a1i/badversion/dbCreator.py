2e2b6e20d9fbabfc1300bcf2c4a9b9c7c2d7d8c8
AshleyRegan <AshleyRegan@users.noreply.github.com>
2015-12-02 17:16:07 -0500

    populating table some more

--- a1i/badversion/dbCreator.py
+++ a1i/badversion/dbCreator.py
@@ -10,6 +10,9 @@ con = lite.connect('people.db')
 with con:
     cur = con.cursor()   
     #creates table & labels column ids
-    cur.execute("CREATE TABLE Ppl(username TEXT, password TEXT)")
+    cur.execute("CREATE TABLE Ppl(id INT, username TEXT, password TEXT)")
     #populating the table
+    cur.execute('''INSERT INTO Ppl VALUES("user", "pass")''')
+    cur.execute('''INSERT INTO Ppl VALUES("admin", "123")''')
+    cur.execute('''INSERT INTO Ppl VALUES("can", "sux")''')
     cur.execute('''INSERT INTO Ppl VALUES("johnny", "you")''')
